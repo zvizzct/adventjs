4b15de7c3156f6ec218b0ef79c3b28020adb7943
zvizzct <victor.puerta01@estudiant.upf.edu>
2023-12-06 23:54:36 +0100

    day 05 - cyberReindeer

---
+++ cyberReindeer.js
@@ -0,0 +1,59 @@
+/*
+Santa 🎅 está probando su nuevo trineo eléctrico, el CyberReindeer, en una carretera
+del Polo Norte. La carretera se representa con una cadena de caracteres, donde:
+- . = Carretera
+- S = Trineo de Santa
+- * = Barrera abierta
+- | = Barrera cerrada
+Ejemplo de carretera: S...|....|.....
+Cada unidad de tiempo, el trineo avanza una posición a la derecha. Si encuentra una barrera 
+cerrada, se detiene hasta que la barrera se abra. Si está abierta, la atraviesa directamente.
+Todas las barreras empiezan cerradas, pero después de 5 unidades de tiempo, se abren todas
+para siempre.
+Crea una función que simule el movimiento del trineo durante un tiempo dado y devuelva un 
+array de cadenas representando el estado de la carretera en cada unidad de tiempo:
+
+El resultado es un array donde cada elemento muestra la carretera en cada unidad de tiempo.
+Ten en cuenta que si el trineo está en la misma posición que una barrera, entonces toma su 
+lugar en el array.
+Los elfos se inspiraron en este reto de Code Wars.
+*/
+
+function cyberReindeer(road, time) {
+  let newRoad = road
+  const roadStates = [road]
+  let position = 0
+  for (let t = 1; t < time; t++) {
+    if (t === 5) {
+      road = road.replaceAll('|', '*')
+    }
+    if (road[position + 1] !== '|') {
+      const before = '.' + road.substring(1, position + 1)
+      const after = road.substring(position + 2, road.length)
+      newRoad = `${before}S${after}`
+      position++
+    }
+    roadStates.push(newRoad)
+  }
+  console.log(roadStates)
+  return roadStates
+}
+
+const road = 'S..|...|..'
+const time = 10 // unidades de tiempo
+const result = cyberReindeer(road, time)
+
+/* -> result:
+[
+  'S..|...|..', // estado inicial
+  '.S.|...|..', // avanza el trineo la carretera
+  '..S|...|..', // avanza el trineo la carretera
+  '..S|...|..', // el trineo para en la barrera
+  '..S|...|..', // el trineo para en la barrera
+  '...S...*..', // se abre la barrera, el trineo avanza
+  '...*S..*..', // avanza el trineo la carretera
+  '...*.S.*..', // avanza el trineo la carretera
+  '...*..S*..', // avanza el trineo la carretera
+  '...*...S..', // avanza por la barrera abierta
+]
+*/
